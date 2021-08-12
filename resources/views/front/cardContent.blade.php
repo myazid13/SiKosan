2ca272c48213a7b4b0a083a43d2d0e5e7299daca
andes2912 <andridesmana29@gmail.com>
2021-08-12 21:47:18 +0700

    update frontend

--- resources/views/front/cardContent.blade.php
+++ resources/views/front/cardContent.blade.php
@@ -10,14 +10,14 @@
             <a href="{{url('room', $kamars->slug)}}">
               <h5>{{$kamars->nama_kamar}}</h5>
               <div class="d-flex-justify-content-between">
-                <span class="badge badge-light">{{$kamars->jenis_kamar}}</span>
-                <span class="badge badge-info">Tersisa {{$kamars->sisa_kamar}} Kamar</span>
+                <a href="" class="btn gradient-light-primary btn-sm">{{$kamars->jenis_kamar}}</a>
+                <a href="#" class="btn btn-outline-{{$kamars->sisa_kamar > 5 ? 'primary' : 'danger'}} btn-sm {{$kamars->sisa_kamar > 5 ? 'primary' : 'danger'}}">Tersisa {{$kamars->sisa_kamar}} kamar</a>
               </div>
-              <p class="card-text mt-1 mb-0"><i class="feather icon-map-pin"></i>{{$kamars->provinsi->name}}</p>
+              <p class="card-text mt-1 mb-0"><i class="feather icon-map-pin"></i> {{$kamars->provinsi->name}}</p>
               <span class="card-text" style="color: black"> {{rupiah($kamars->harga_kamar)}} / Bulan</span>
             </a>
             <div class="card-btn d-flex justify-content-between mt-2">
-              <a href="#" class="btn gradient-light-{{$kamars->kategori == 'Kost' ? 'primary' : 'info'}} text-white btn-sm">{{$kamars->kategori}}</a>
+              <a href="#" class="btn gradient-light-{{$kamars->kategori == 'Kost' ? 'warning' : 'info'}} text-white btn-sm">{{$kamars->kategori}}</a>
               <a href="#" class="btn btn-outline-primary btn-sm {{$kamars->book == 0 ? 'hidden' : ''}}">Bisa Booking</a>
             </div>
           </div>
