60aa00594372e29fe312738e7afe5350507fb5c7
andes2912 <andridesmana29@gmail.com>
2021-08-13 20:17:01 +0700

    Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,145 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-  <style>
-    .border-md {
-        border-width: 2px;
-    }
+@extends('layouts.auth')
 
-    .btn-facebook {
-        background: #405D9D;
-        border: none;
-    }
-
-    .btn-facebook:hover, .btn-facebook:focus {
-        background: #314879;
-    }
-
-    .btn-twitter {
-        background: #42AEEC;
-        border: none;
-    }
-
-    .btn-twitter:hover, .btn-twitter:focus {
-        background: #1799e4;
-    }
-
-    body {
-        min-height: 100vh;
-    }
-
-    .form-control:not(select) {
-        padding: 1.5rem 0.5rem;
-    }
-
-    select.form-control {
-        height: 52px;
-        padding-left: 0.5rem;
-    }
-
-    .form-control::placeholder {
-        color: #ccc;
-        font-weight: bold;
-        font-size: 0.9rem;
-    }
-    .form-control:focus {
-        box-shadow: none;
-    }
-
-  </style>
-</head>
-<body>
-  <!-- Navbar-->
-<header class="header">
-    <nav class="navbar navbar-expand-lg navbar-light py-3">
-        <div class="container">
-            <!-- Navbar Brand -->
-            <a href="/" class="navbar-brand">
-                <h3>Pap!Kos</h3>
-            </a>
-        </div>
-    </nav>
-</header>
-
-
-<div class="container">
-  <div class="row py-5 mt-4 align-items-center">
-    <div class="col-md-5 pr-lg-5 mb-5 mb-md-0">
-      <img src="https://res.cloudinary.com/mhmd/image/upload/v1569543678/form_d9sh6m.svg" alt="" class="img-fluid mb-3 d-none d-md-block">
-      <h1>Login Account</h1>
-      <p class="font-italic text-muted mb-0">Cari Kost dan Apartement Makin Mudah di Pap!Kos</p>
-    </div>
-
-    <!-- Registeration Form -->
-    <div class="col-md-7 col-lg-6 ml-auto">
-      <form action="{{route('login')}}" method="POST">
-        @csrf
-        <div class="row">
-
-          <!-- Email Address -->
-          <div class="input-group col-lg-12 mb-4">
-            <div class="input-group-prepend">
-                <span class="input-group-text bg-white px-4 border-md border-right-0">
-                    <i class="fa fa-envelope text-muted"></i>
-                </span>
+@section('title')
+  Pap!Kos - Login Page
+@endsection
+@section('content')
+<div class="col-lg-6 col-12 p-0">
+    <div class="card rounded-0 mb-0 px-2">
+        <div class="card-header pb-1">
+            <div class="card-title">
+                <h4 class="mb-0" style="text-align: center">Cari Kost dan Apartement Makin Mudah di Pap!Kos</h4>
             </div>
-            <input id="email" type="email" name="email" placeholder="Email Address" class="form-control bg-white border-left-0 border-md @error('email') is-invalid @enderror" value="{{ old('email') }}">
-            @error('email')
-              <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-              </span>
-            @enderror
-          </div>
-
-          <!-- Password -->
-          <div class="input-group col-lg-12 mb-4">
-            <div class="input-group-prepend">
-              <span class="input-group-text bg-white px-4 border-md border-right-0">
-                <i class="fa fa-lock text-muted"></i>
-              </span>
-            </div>
-            <input id="password" type="password" name="password" placeholder="Password" class="form-control bg-white border-left-0 border-md @error('password') is-invalid @enderror">
-            @error('password')
-              <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-              </span>
-            @enderror
-          </div>
-
-          <!-- Submit Button -->
-          <div class="form-group col-lg-12 mx-auto mb-0">
-            <button type="submit" class="btn btn-primary btn-block py-2">
-              <span class="font-weight-bold">Login your account</span>
-            </button>
-          </div>
-
-          <!-- Not Already Registered -->
-          <div class="text-center w-100">
-            <p class="text-muted font-weight-bold">Not Already Registered? <a href="{{route('register')}}" class="text-primary ml-2">Register</a></p>
-          </div>
-
         </div>
-      </form>
+        {{-- <p class="px-5">Selamat datang kembali, silahkan login untuk masuk ke akun kamu.</p> --}}
+        <div class="card-content mb-2">
+            <div class="card-body pt-1">
+                <form action="{{route('login')}}" method="POST">
+                  @csrf
+                    <fieldset class="form-label-group form-group position-relative has-icon-left">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Masukan Email Kamu ...">
+                        @error('email')
+                          <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                          </span>
+                        @enderror
+                        <div class="form-control-position">
+                            <i class="feather icon-user"></i>
+                        </div>
+                        <label for="user-name">Email</label>
+                    </fieldset>
+
+                    <fieldset class="form-label-group position-relative has-icon-left">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
+                        @error('password')
+                          <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                          </span>
+                        @enderror
+                        <div class="form-control-position">
+                            <i class="feather icon-lock"></i>
+                        </div>
+                        <label for="user-password">Password</label>
+                    </fieldset>
+                    <div class="form-group d-flex justify-content-between align-items-center">
+                        <div class="text-left">
+                            <fieldset class="checkbox">
+                                <div class="vs-checkbox-con vs-checkbox-primary">
+                                    <input type="checkbox">
+                                    <span class="vs-checkbox">
+                                        <span class="vs-checkbox--check">
+                                            <i class="vs-icon feather icon-check"></i>
+                                        </span>
+                                    </span>
+                                    <span class="">Remember me</span>
+                                </div>
+                            </fieldset>
+                        </div>
+                        <div class="text-right"><a href="/" class="card-link">Lupa Password ?</a></div>
+                    </div>
+                    <a href="{{route('register')}}" class="btn btn-outline-primary float-left btn-inline">Register</a>
+                    <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
+                </form>
+            </div>
+        </div>
+        <div class="login-footer">
+            <div class="divider">
+                <div class="divider-text"><i class="feather icon-home"></i></div>
+            </div>
+            <div class="footer-btn d-inline">
+                {{-- <a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
+                <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
+                <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
+                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a> --}}
+                <a href="/"><h5 style="text-align: center">Kembali</h5></a>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
-</body>
-<script>
-  $(function () {
-    $('input, select').on('focus', function () {
-        $(this).parent().find('.input-group-text').css('border-color', '#80bdff');
-    });
-    $('input, select').on('blur', function () {
-        $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
-    });
-  });
-</script>
-</html>
+@endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,89 +1,92 @@
 <!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="en">
-<!--<![endif]-->
-<head>
-	<meta charset="utf-8" />
-	<title>@yield('title')</title>
-	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
-	<meta content="" name="description" />
-	<meta content="" name="author" />
-	
-	<!-- ================== BEGIN BASE CSS STYLE ================== -->
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-	<link href="{{asset('backend/plugins/jquery-ui/jquery-ui.min.css')}}" rel="stylesheet" />
-	<link href="{{asset('backend/plugins/bootstrap/4.0.0/css/bootstrap.min.css')}}" rel="stylesheet" />
-	<link href="{{asset('backend/plugins/font-awesome/5.0/css/fontawesome-all.min.css')}}" rel="stylesheet" />
-	<link href="{{asset('backend/plugins/animate/animate.min.css')}}" rel="stylesheet" />
-	<link href="{{asset('backend/css/default/style.min.css')}}" rel="stylesheet" />
-	<link href="{{asset('backend/css/default/style-responsive.min.css')}}" rel="stylesheet" />
-	<link href="{{asset('backend/css/default/theme/default.css')}}" rel="stylesheet" id="theme" />
-	<!-- ================== END BASE CSS STYLE ================== -->
-	
-	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="{{asset('backend/plugins/pace/pace.min.js')}}"></script>
-	<!-- ================== END BASE JS ================== -->
-</head>
-<body class="pace-top">
-	<!-- begin #page-loader -->
-	<div id="page-loader" class="fade show"><span class="spinner"></span></div>
-	<!-- end #page-loader -->
-	
-	<div class="login-cover">
-	    <div class="login-cover-image" style="background-image: url({{asset('backend/img/login-bg/login-bg-12.jpg')}})" data-id="login-cover-image"></div>
-	    <div class="login-cover-bg"></div>
-	</div>
-	<!-- begin #page-container -->
-	<div id="page-container" class="fade">
-	    <!-- begin login -->
-        @yield('auth')
-        <!-- end login -->
-        
-        <ul class="login-bg-list clearfix">
-            <li><a href="javascript:;" data-click="change-bg" data-img="{{asset(
-                'backend/img/login-bg/login-bg-17.jpg')}}" style="background-image: url({{asset(
-                'backend/img/login-bg/login-bg-17.jpg')}})"></a></li>
-            <li><a href="javascript:;" data-click="change-bg" data-img="{{asset(
-                'backend/img/login-bg/login-bg-16.jpg')}}" style="background-image: url({{asset(
-                'backend/img/login-bg/login-bg-16.jpg')}})"></a></li>
-            <li><a href="javascript:;" data-click="change-bg" data-img="{{asset(
-                'backend/img/login-bg/login-bg-15.jpg')}}" style="background-image: url({{asset(
-                'backend/img/login-bg/login-bg-15.jpg')}})"></a></li>
-            <li><a href="javascript:;" data-click="change-bg" data-img="{{asset(
-                'backend/img/login-bg/login-bg-14.jpg')}}" style="background-image: url({{asset(
-                'backend/img/login-bg/login-bg-14.jpg')}})"></a></li>
-            <li><a href="javascript:;" data-click="change-bg" data-img="{{asset(
-                'backend/img/login-bg/login-bg-13.jpg')}}" style="background-image: url({{asset(
-                'backend/img/login-bg/login-bg-13.jpg')}})"></a></li>
-            <li class="active"><a href="javascript:;" data-click="change-bg" data-img="{{asset(
-                'backend/img/login-bg/login-bg-12.jpg')}}" style="background-image: url({{asset(
-                'backend/img/login-bg/login-bg-12.jpg')}})"></a></li>
-        </ul>
-        
-	</div>
-	<!-- end page container -->
-	
-	<!-- ================== BEGIN BASE JS ================== -->
-	<script src="{{asset('backend/plugins/jquery/jquery-3.2.1.min.js')}}"></script>
-	<script src="{{asset('backend/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-	<script src="{{asset('backend/plugins/bootstrap/4.0.0/js/bootstrap.bundle.min.js')}}"></script>
-	<script src="{{asset('backend/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
-	<script src="{{asset('backend/plugins/js-cookie/js.cookie.js')}}"></script>
-	<script src="{{asset('backend/js/theme/default.min.js')}}"></script>
-	<script src="{{asset('backend/js/apps.min.js')}}"></script>
-	<!-- ================== END BASE JS ================== -->
-	
-	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
-	<script src="{{asset('backend/js/demo/login-v2.demo.min.js')}}"></script>
-	<!-- ================== END PAGE LEVEL JS ================== -->
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
 
-	<script>
-		$(document).ready(function() {
-			App.init();
-			LoginV2.init();
-		});
-    </script>
-    @yield('script')
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>@yield('title')</title>
+    <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/css/vendors.min.css')}}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/themes/semi-dark-layout.css')}}">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/core/menu/menu-types/horizontal-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/core/colors/palette-gradient.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/pages/authentication.css')}}">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    {{-- <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css"> --}}
+    <!-- END: Custom CSS-->
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="horizontal-layout horizontal-menu 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="hover" data-menu="horizontal-menu" data-col="1-column">
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+              <section class="row flexbox-container">
+                <div class="col-xl-8 col-11 d-flex justify-content-center">
+                  <div class="card bg-authentication rounded-0 mb-0">
+                            <div class="row m-0">
+                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                                    <img src="{{asset('assets/images/pages/login.png')}}" alt="branding logo">
+                                </div>
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{asset('assets/vendors/js/vendors.min.js')}}"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{asset('assets/vendors/js/ui/jquery.sticky.js')}}"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="{{asset('assets/js/core/app-menu.js')}}"></script>
+    <script src="{{asset('assets/js/core/app.js')}}"></script>
+    <script src="{{asset('assets/js/scripts/components.js')}}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <!-- END: Page JS-->
+
 </body>
+<!-- END: Body-->
+
 </html>
