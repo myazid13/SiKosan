c38383a3bae44ebac9ea1476eba2d74da73c248e
andes2912 <andridesmana29@gmail.com>
2021-08-17 01:55:30 +0700

    change image kos by kota

--- resources/views/front/byKota.blade.php
+++ resources/views/front/byKota.blade.php
@@ -1,7 +1,7 @@
 <style>
   .title_kota {
-    background-color: rgb(223, 215, 215);
-    opacity: 0.7;
+    /* background-color: rgb(223, 215, 215);
+    opacity: 0.7; */
   }
 </style>
 <section class="ftco-section testimony-section">
@@ -10,9 +10,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=DKI+JAKARTA')}}">
           <div class="card shadow">
-            <img src="https://cdn.pixabay.com/photo/2020/02/29/17/14/wallpaper-4890663_1280.jpg" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/jakarta.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Jakarta</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Jakarta</h5>
             </div>
           </div>
         </a>
@@ -21,9 +21,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=DI+YOGYAKARTA')}}">
           <div class="card shadow">
-            <img src="https://cdn.pixabay.com/photo/2019/12/29/16/54/yogyakarta-4727525_1280.png" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/jogja.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Yogyakarta</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Yogyakarta</h5>
             </div>
           </div>
         </a>
@@ -32,9 +32,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=KOTA+SURABAYA')}}">
           <div class="card shadow">
-            <img src="{{asset('frontend/images/icon/surabaya.webp')}}" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/surabaya.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Surabaya</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Surabaya</h5>
             </div>
           </div>
         </a>
@@ -43,9 +43,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=KABUPATEN+BANDUNG')}}">
           <div class="card shadow">
-            <img src="https://img.pngio.com/bandung-bandung-city-building-city-indonesian-monument-bandung-png-512_512.png" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/bandung.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Bandung</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Bandung</h5>
             </div>
           </div>
         </a>
@@ -54,9 +54,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=KABUPATEN+MALANG')}}">
           <div class="card shadow">
-            <img src="{{asset('frontend/images/icon/malang.jpeg')}}" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/malang.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Malang</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Malang</h5>
             </div>
           </div>
         </a>
@@ -65,9 +65,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=KABUPATEN+SEMARANG')}}">
           <div class="card shadow">
-            <img src="{{asset('frontend/images/icon/semarang.png')}}" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/semarang.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Semarang</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Semarang</h5>
             </div>
           </div>
         </a>
@@ -76,9 +76,9 @@
       <div class="col-md-3 mb-4">
         <a href="{{url('kost/?kota=KOTA+MEDAN')}}">
           <div class="card shadow">
-            <img src="https://cdn.dribbble.com/users/3836120/screenshots/6917080/masjid_raya_finish.jpg" class="card-img" height="180px" alt="...">
+            <img src="{{asset('assets/images/city/medan.png')}}" class="card-img" height="180px" alt="...">
             <div class="card-img-overlay">
-              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Kos Medan</h5>
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Kos Medan</h5>
             </div>
           </div>
         </a>
@@ -90,7 +90,7 @@
         <div class="card shadow">
           <img src="https://cdn.pixabay.com/photo/2018/02/17/14/15/pattern-3160023_1280.jpg" class="card-img" height="180px" alt="...">
           <div class="card-img-overlay">
-            <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:15px">Lihat Semua</p>
+            <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:black; font-weight:bold; font-size:20px">Lihat Semua<i class="feather icon-arrow-right"></i> </p>
           </div>
         </div>
        </a>
