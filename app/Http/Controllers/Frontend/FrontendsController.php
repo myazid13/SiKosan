67c2935816821473ce44f6c8dc623e5d95ca5043
andes2912 <andridesmana29@gmail.com>
2021-09-19 10:00:09 +0700

    Improve transaction

--- app/Http/Controllers/Frontend/FrontendsController.php
+++ app/Http/Controllers/Frontend/FrontendsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\{kamar,provinsi,Testimoni,User,SimpanKamar};
 use Auth;
+
 class FrontendsController extends Controller
 {
     // Homepage
